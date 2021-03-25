69bfab14f2e61d80cf433f04942acbf01333af7f
george.scott <george.scott@sage.com>
2021-03-25 15:28:59 +0000

    example base types and method definition

--- Program.cs
+++ Program.cs
@@ -26,12 +26,20 @@ namespace dotnet_roslyn_code_generation
             @namespace = @namespace.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System")));
 
             //  Create a class: (get uses command interface)
-            var interfaceDefinition = SyntaxFactory.InterfaceDeclaration("IGetUsersCommand");
-
-            interfaceDefinition = interfaceDefinition.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+            var interfaceDefinition = SyntaxFactory.InterfaceDeclaration("IGetUsersCommand")
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                .AddBaseListTypes(SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("ICustomCommand<UpdateBusinessHealthCommandRequest, UpdateBusinessHealthCommandResponse>")));
 
             // Create a stament with the body of a method.
             var syntax = SyntaxFactory.ParseStatement("canceled = true;");
+
+            // Create a method
+            var methodDeclaration = SyntaxFactory.MethodDeclaration(SyntaxFactory.ParseTypeName("void"), "MarkAsCanceled")
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken));
+
+            // Add the field, the property and method to the class.
+            interfaceDefinition = interfaceDefinition.AddMembers(methodDeclaration);
 
             // Add the class to the namespace.
             @namespace = @namespace.AddMembers(interfaceDefinition);

818ebe317894106c8ce109456a5da99cfe71d66c
george.scott <george.scott@sage.com>
2021-03-26 15:56:24 +0000

    use temp static class

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
+using dotnet_roslyn_code_generation.commands;
 
 namespace dotnet_roslyn_code_generation
 {
@@ -11,43 +10,10 @@ namespace dotnet_roslyn_code_generation
             Console.WriteLine("Hello World!");
 
             // Create a command interface
-            CreateCommandInterface();
+            CommandCreator.CreateCommandInterface();
 
             // Wait to exit.
             Console.Read();
-        }
-
-        private static void CreateCommandInterface()
-        {
-            // Create a namespace: (namespace CodeGenerationSample)
-            var @namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName("projectnamespacecore")).NormalizeWhitespace();
-
-             // Add System using statement: (using System)
-            @namespace = @namespace.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System")));
-
-            //  Create a class: (get uses command interface)
-            var interfaceDefinition = SyntaxFactory.InterfaceDeclaration("IGetUsersCommand")
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-                .AddBaseListTypes(SyntaxFactory.SimpleBaseType(SyntaxFactory.ParseTypeName("ICustomCommand<UpdateBusinessHealthCommandRequest, UpdateBusinessHealthCommandResponse>")));
-
-            // Create a method
-            var methodDeclaration = SyntaxFactory.MethodDeclaration(SyntaxFactory.ParseTypeName("void"), "MarkAsCanceled")
-                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
-                .WithSemicolonToken(SyntaxFactory.Token(SyntaxKind.SemicolonToken));
-
-            // Add the field, the property and method to the class.
-            interfaceDefinition = interfaceDefinition.AddMembers(methodDeclaration);
-
-            // Add the class to the namespace.
-            @namespace = @namespace.AddMembers(interfaceDefinition);
-
-            // Normalize and get code as string.
-            var code = @namespace
-                .NormalizeWhitespace()
-                .ToFullString();
-
-            // Output new code to the console.
-            Console.WriteLine(code);
         }
     }
 }

c50fe241c05cf31caf09d73fe4904409b149daf7
george.scott <george.scott@sage.com>
2021-04-19 15:44:09 +0100

    test interface builder

--- CodeGen.Test/InterfaceBuilderTests.cs
+++ CodeGen.Test/InterfaceBuilderTests.cs
@@ -1,25 +1,52 @@
 using System;
 using Xunit;
 using codegen.library.builders;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using codegen.library.definitions;
 
 namespace codegen.Test
 {
     public class InterfaceBuilderTests
     {
         [Fact]
-        public void WhenDefinitionIsProvided_AndInterfaceIsBuilt_DefinitionIsPresent()
+        public void WhenDefinitionIsProvided_WhenInterfaceIsBuilt_DefinitionIsPresent()
         {
             var name = "test";
             var baseclass = "testBase";
             TypeDeclarationSyntax interfaceResult = new InterfaceBuilder()
                         .WithDefinition(name, baseclass)
-                        // .WithSummary(interfaceDefinition.Summary())
-                        // .WithMethodDeclarations(interfaceDefinition.MethodDeclarations())
                         .Build();
 
-            Assert.Equal(interfaceResult.ToFullString(), $"publicinterface{name}:{baseclass}{{}}");
+            Assert.Equal($"publicinterface{name}:{baseclass}{{}}", interfaceResult.ToFullString());
+        }
+
+        [Fact]
+        public void WhenSummaryIsProvided_WhenInterfaceIsBuilt_SummaryIsPresent()
+        {
+            var summary = "interface to test";
+            TypeDeclarationSyntax interfaceResult = new InterfaceBuilder()
+                        .WithDefinition("test", "testBase")
+                        .WithSummary(summary)
+                        .Build();
+
+            Assert.StartsWith($"/// <summary>/// {summary}/// </summary>", interfaceResult.ToFullString());
+        }
+
+        [Fact]
+        public void WhenMethodIsProvided_WhenInterfaceIsBuilt_MethodIsPresent()
+        {
+            var method = new MethodDeclaration()
+                {
+                    Name = "TimeoutAfter", 
+                    Type = "TimeSpan"
+                };
+
+            TypeDeclarationSyntax interfaceResult = new InterfaceBuilder()
+                        .WithDefinition("test", "testBase")
+                        .WithMethodDeclarations(new MethodDeclaration[] { method })
+                        .Build();
+            Console.WriteLine(interfaceResult);
+            Assert.EndsWith($"{method.Type}{method.Name}();}}", interfaceResult.ToFullString());
         }
     }
 }
